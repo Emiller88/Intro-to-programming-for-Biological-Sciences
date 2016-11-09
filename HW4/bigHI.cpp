d5c66d28c0364b48d9e5c5fcf41fd9b3e5b2e70b
Edmund Miller <edmund.a.miller@protonmail.com>
2016-11-09 17:17:05 +0100

    Update HW4/bigHI.cpp - 1478708225

---
+++ HW4/bigHI.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main ( )
+{
+  char resp;
+  int i,j,y,x;
+  int width,height;
+
+  initscr();
+  getmaxyx(stdscr,height,width);
+  clear();
+
+  box(stdscr,'*','-');
+  move(1,2);  // goto row 1 column 2
+  printw("Print BIG letters");
+  move(1,width-10);
+  printw("10/13/16");
+  /* send stdscr to terminal  */
+  refresh();
+
+  cbreak(); // make characters immediately accessible
+  noecho(); // don't echo inputs
+
+  move(height-2,2);
+  printw("Hit any key to see what happens next:  ");
+  getyx(stdscr,y,x);
+  move(y,x-2);
+
+  refresh();
+  resp = getch();
+  // draw big H
+  for (i=3; i < 22 ; i++ )
+  {
+    move(i,15);
+    printw("Hello");
+    move(i,35);
+    printw("Hello");
+    if ((10 < i) && (i < 14))
+    {
+      move(i,20);
+      for (j=20; j<35; j+=5)
+        printw("Hello");
+    }
+  }
+  refresh();
+
+
+  move(height-2,2);
+  printw("Hit any key to see what happens next:  ");
+  getyx(stdscr,y,x);
+  move(y,x-2);
+  refresh();
+  resp = getch();
+
+  // draw big i
+  for (i=3; i < 22 ; i++ )
+  {
+    move(i,55);
+    printw("Hello");
+    if (((3 <= i) && (i <= 5)) || ((19 <= i) && (i <= 21)))
+    {
+      move(i,50);
+      for (j=50; j<65; j+=5)
+        printw("Hello");
+    }
+  }
+
+  move(height-2,2);
+  printw("Hit any key to exit:                   ");
+  move(height-2,22);
+  resp = getch();
+
+  endwin();
+
+  return 0;
+}
